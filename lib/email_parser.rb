ce4868a7824974357df1457d536906dd627fa8d2
cedric e goddard <8ceg6@comcast.net>
2020-01-11 01:01:49 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,6 +10,7 @@ class EmailAddressParser
       parsed_emails= emails.split.collect do |address|
         address.split(",").join
       end 
+      binding.pry 
      parsed_emails.uniq
   end 
 end 
